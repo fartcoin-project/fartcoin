301a03feb0aea471c717b9adf9a989a6ee0377d1
fartcoin <fartcoinproject@gmail.com>
2019-02-20 19:58:02 +0100

    version update

--- src/version.cpp
+++ src/version.cpp
@@ -10,10 +10,10 @@
 // Name of client reported in the 'version' message. Report the same name
 // for both fartcoind and fartcoin-qt, to make it harder for attackers to
 // target servers or GUI users specifically.
-const std::string CLIENT_NAME("Flatulence");
+const std::string CLIENT_NAME("Space_Police");
 
 // Client version number
-#define CLIENT_VERSION_SUFFIX   "0.9"
+#define CLIENT_VERSION_SUFFIX   "0.9.2"
 
 
 // The following part of the code determines the CLIENT_BUILD variable.
