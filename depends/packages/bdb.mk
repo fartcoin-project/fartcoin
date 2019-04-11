241745367a57e9eb24e1988f206c95504c6e0519
fartcoin-project <fartcoinproject@gmail.com>
2019-04-11 20:38:57 +0200

    Update Depends to working OSX Maverick setup

--- depends/packages/bdb.mk
+++ depends/packages/bdb.mk
@@ -1,6 +1,6 @@
 package=bdb
 $(package)_version=5.1.29
-$(package)_download_path=http://download.oracle.com/berkeley-db
+$(package)_download_path=http://download.oracle.com/berkeley-db/
 $(package)_file_name=db-$($(package)_version).NC.tar.gz
 $(package)_sha256_hash=08238e59736d1aacdd47cfb8e68684c695516c37f4fbe1b8267dde58dc3a576c
 $(package)_build_subdir=build_unix
@@ -9,14 +9,11 @@ define $(package)_set_vars
 $(package)_config_opts=--disable-shared --enable-cxx --disable-replication
 $(package)_config_opts_mingw32=--enable-mingw
 $(package)_config_opts_linux=--with-pic
-$(package)_cxxflags=-std=c++11
 endef
 
 define $(package)_preprocess_cmds
   sed -i.old 's/__atomic_compare_exchange/__atomic_compare_exchange_db/' src/dbinc/atomic.h && \
-  sed -i.old 's/atomic_init/atomic_init_db/' src/dbinc/atomic.h src/mp/mp_region.c src/mp/mp_mvcc.c src/mp/mp_fget.c src/mutex/mut_method.c src/mutex/mut_tas.c && \
-  sed -i.old 's/WinIoCtl\.h/winioctl\.h/g' src/dbinc/win_db.h && \
-  cp -f $(BASEDIR)/config.guess $(BASEDIR)/config.sub dist
+  sed -i.old 's/WinIoCtl\.h/winioctl\.h/g' src/dbinc/win_db.h
 endef
 
 define $(package)_config_cmds
